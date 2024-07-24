3843fb3e36877c70059a82b3f871723943a97433
Alexandr Kotenko <kpshmt@gmail.com>
2024-07-24 21:49:32 +0300

    upd final

--- src/FooterJoinLink.js
+++ src/FooterJoinLink.js
@@ -4,7 +4,7 @@ import styles from './FooterJoinLink.module.css';
 
 const FooterJoinLink = () => {
   return (
-    <Link to="/thankyoupage" className={styles.footerJoinLink}>
+    <Link to="https://melbourne-30.com/thankyoupage.html" className={styles.footerJoinLink}>
       JOIN CR30WN
     </Link>
   );
--- src/LoginLink.js
+++ src/LoginLink.js
@@ -4,7 +4,7 @@ import styles from './LoginLink.module.css';
 
 const LoginLink = () => {
   return (
-    <Link to="/thankyoupage" className={styles.loginLink}>
+    <Link to="https://melbourne-30.com/thankyoupage.html" className={styles.loginLink}>
       LOGIN
     </Link>
   );
